8fbebb5b04bc4472be916d9aff0facb89c3c061d
Leonardo Zadorosny <leozadorosny0@gmail.com>
2016-05-12 02:09:35 -0300

    Atualização

--- IMCcalc/src/main/java/mvc/IMCController.java
+++ IMCcalc/src/main/java/mvc/IMCController.java
@@ -1,5 +1,20 @@
 package mvc;
 
-public class IMCController {
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/imc")
+public class IMCController extends HttpServlet{
+     
+}
+
+@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 
 }
+
+
